6fe4eb1731fb1f16df4ab87f2411629651afcfc2
Nadia Bey <nadia.bey@duke.edu>
2021-11-08 08:18:04 -0500

    updated mergequery

--- mergequery.py
+++ mergequery.py
@@ -16,7 +16,7 @@ def getdb():
               ") SELECT details.course_num, dept, catalog_num, course_name, topic, section_type, section_num,"
               "open_seats, total_seats, reserves, percentage, time_recorded FROM details, sections, names "
               "WHERE names.course_num = details.course_num AND sections.course_num = details.course_num"
-              "AND dept = " + x + ";")
+              " AND dept = " + x + ";")
 
 
 if __name__ == '__main__':
