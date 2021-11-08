b02201fcc2956e7ed0330befce3e09649f3600c1
Nadia Bey <nadia.bey@duke.edu>
2021-11-08 08:21:07 -0500

    updated mergequery/output

--- mergequery.py
+++ mergequery.py
@@ -10,13 +10,13 @@ def getdb():
     lines = ["SELECT * FROM {}".format(x) for x in depts]
     print(".headers on\n.mode csv")
     for x in lines:
-        name = x[x.find("FROM") + 4:]
-        print(".output" + name + ".csv")
+        name = x[x.find("FROM") + 5:]
+        print(".output " + name + ".csv")
         print("WITH details(course_num, open_seats, total_seats, reserves, percentage, time_recorded) AS (" + x +
               ") SELECT details.course_num, dept, catalog_num, course_name, topic, section_type, section_num,"
               "open_seats, total_seats, reserves, percentage, time_recorded FROM details, sections, names "
               "WHERE names.course_num = details.course_num AND sections.course_num = details.course_num"
-              " AND dept = " + x + ";")
+              " AND dept = '" + name + "';")
 
 
 if __name__ == '__main__':
